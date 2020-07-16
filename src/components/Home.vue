8cd886bb7aa349d7ab4bf94b2115838edad6faca
2218675712 <2218675712@qq.com>
2020-07-16 18:29:23 +0800

    token销毁

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <el-row>
+      <el-button type="danger" round @click="logout">退出登录</el-button>
+    </el-row>
     我是home
   </div>
 
@@ -8,6 +11,13 @@
 <script>
 export default {
   // name: 'Home'
+  methods: {
+    logout () {
+      // 销毁本地token
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
