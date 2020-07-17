e43ba8d68c407f898b584d5ae22bbac243afd866
2218675712 <2218675712@qq.com>
2020-07-17 11:47:53 +0800

    侧边栏完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,21 +8,13 @@
           <el-menu
             default-active="2"
             class="el-menu-vertical-demo">
-            <el-submenu index="1">
+            <el-submenu :index="item.id+''" :key="item.id" v-for="item in menuList">
               <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <i :class="iconObj[item.id]"></i>
+                <span>{{item.authName}}</span>
               </template>
               <el-menu-item-group>
-                <template slot="title">
-                  <i class="el-icon-location"></i>
-                  分组一
-                </template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
+                <el-menu-item :index="subItem.id+''" :key="subItem.id" v-for="subItem in item.children">{{subItem.authName}}</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
           </el-menu>
@@ -53,12 +45,39 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // 侧边栏数据
+      menuList: [],
+      // 侧边栏图标
+      iconObj: {
+        125: 'el-icon-user',
+        103: 'el-icon-coordinate',
+        101: 'el-icon-shopping-cart-full',
+        102: 'el-icon-document',
+        145: 'el-icon-coin'
+      }
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   // name: 'Home'
   methods: {
+    /**
+     * 退出登录
+     */
     logout () {
       // 销毁本地token
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      // 返回错误信息
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import './plugins/element.js'
 import './assets/css/globle.css'
 
 import axios from 'axios'
+// 添加请求拦截器添加token
+axios.interceptors.request.use((config) => {
+  config.headers.authorization = window.sessionStorage.getItem('token')
+  return config
+})
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
